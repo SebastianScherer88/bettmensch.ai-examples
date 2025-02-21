60e383acf3106d2182260eacd49fb460307e7522
SebastianScherer88 <scherersebastian@yahoo.de>
2025-02-21 16:17:03 +0000

    refactored pretrain function by adding a trainer class and a config

--- sdk/bettmensch_ai_examples/gpt_1/src/train.py
+++ sdk/bettmensch_ai_examples/gpt_1/src/train.py
@@ -1,7 +1,8 @@
 import pickle
 import sys
 from datetime import datetime as dt
-from typing import Any, List, Tuple
+import yaml
+from typing import Any, List, Tuple, Dict, Optional, Literal
 
 import GPUtil
 import torch
@@ -9,7 +10,7 @@ import torch.utils
 import torch.utils.data
 import torch.utils.tensorboard
 import torch.utils.tensorboard.summary
-from sdk.bettmensch_ai_examples.gpt_1.src.model import GPT1Pretrain
+from .model import GPT1Pretrain
 from transformers import OpenAIGPTConfig, OpenAIGPTTokenizerFast
 from jaxtyping import Float
 
@@ -91,196 +92,264 @@ class TokenizedBookCorpusOpenSplit(torch.utils.data.Dataset):
         Float[torch.Tensor, "n_batch n_tokens"],
     ]:
         
-        inputs_list = [torch.tensor(token_sample[:-1], dtype=torch.long) for token_sample in token_samples]
+        inputs_list = [torch.tensor(token_sample[:-1], dtype=torch.int) for token_sample in token_samples]
         inputs_tensor = torch.stack(inputs_list)
 
         mask_tensor = torch.ones(inputs_tensor.size(),dtype=torch.bool)
 
-        target_list = [torch.tensor(token_sample[1:], dtype=torch.long) for token_sample in token_samples]
+        target_list = [torch.tensor(token_sample[1:], dtype=torch.int64) for token_sample in token_samples]
         target_tensor = torch.stack(target_list)
 
         return inputs_tensor, mask_tensor, target_tensor
 
-def train_epoch(
-    epoch_index: int,
-    train_loader: torch.utils.data.DataLoader,
-    validation_loader: torch.utils.data.DataLoader,
-    model: GPT1Pretrain,
-    loss_module: torch.nn.CrossEntropyLoss,
-    optimizer: torch.optim.Optimizer,
-    scheduler: torch.optim.lr_scheduler.LRScheduler,
-    summary_writer: torch.utils.tensorboard.writer.SummaryWriter,
-    display_step: int = 10,
-) -> float:
 
-    model.train()
-    running_loss = 0.0
-    train_step_loss = 0.0
-    n_records = len(train_loader) * train_loader.batch_size
+class GPT1PretrainConfig:
 
-    # Here, we use enumerate(training_loader) instead of
-    # iter(training_loader) so that we can track the batch
-    # index and do some intra-epoch reporting
-    for batch_index, (inputs, mask, target) in enumerate(train_loader):
-        # Every data instance is an input + label pair
-        inputs = inputs.to(0)
-        mask = mask.to(0)
-        target = target.to(0)
+    def __init__(self,config: Dict):
+        self.tokenizer = config['tokenizer']
+        self.data = config['data']
+        self.model = config['model']
+        self.trainer = config['trainer']
+        
+    @classmethod
+    def from_file(cls, file_path: str):
+        with open(file_path,"r") as yaml_file:
+            config_dict = yaml.load(yaml_file)
 
-        # Zero your gradients for every batch!
-        optimizer.zero_grad()
+        return cls(config_dict)
 
-        # Make predictions for this batch
-        outputs = model(inputs, mask)
+class GPT1Trainer:
 
-        # Compute the loss and its gradients
-        loss_node = loss_module(outputs, target)
-        loss_node.backward()
-
-        # update model weights and learning rate
-        optimizer.step()
-        scheduler.step()
-
-        # Gather data and report
-        running_loss += loss_node.item()
-        if batch_index % display_step == (display_step - 1):
-
-            train_step_loss = (
-                running_loss / display_step
-            )  # average loss over last display_step batches
-
-            record_index_start = batch_index * train_loader.batch_size
-            record_index_end = (
-                batch_index + display_step
-            ) * train_loader.batch_size
-            print(
-                f"{now()} | Batch {batch_index+1}/{len(train_loader)}"
-                f" (observations {record_index_start}-{record_index_end}/"
-                f"{n_records}) loss: {train_step_loss}"
-            )
-
-            global_batch_index = (
-                epoch_index * len(train_loader) + batch_index + 1
-            )
-            summary_writer.add_scalar(
-                "Batch train loss", train_step_loss, global_batch_index
-            )
-
-            gpu = GPUtil.getGPUs()[0]
-            summary_writer.add_scalars(
-                "Batch GPU utilization",
-                {"Load": gpu.load, "Memory": gpu.memoryUtil},
-                global_batch_index,
-            )
-
-            running_loss = 0.0
-
-    # calculate validation loss
-    model.eval()
-
-    validation_running_loss = 0
-
-    with torch.no_grad():
-        for batch_index, (inputs_val, mask_val, targets_val) in enumerate(validation_loader):
-            inputs_val = inputs_val.to(0)
-            mask_val = mask_val.to(0)
-            targets_val = targets_val.to(0)
-            output_val = model(inputs_val, mask_val)
-            loss_node = loss_module(output_val, targets_val)
-            validation_running_loss += loss_node.item()
-
-    validation_loss = validation_running_loss / (batch_index + 1)
-
-    return train_step_loss, validation_loss
-
-
-def pretrain(
-    # dataset
-    train_data_path: str,
-    validation_data_path: str,
-    # dataloader
-    batch_size: int,
-    # tokenizer
-    tokenizer_path: str,
-    # model
-    sequence_length: int = 512,
-    dim_embed: int = 768,
-    n_decoder_layers: int = 12,
-    n_heads: int = 12,
-    dropout: float = 0.1,
-    # training
-    n_epochs: int = 10,
-    display_step: int = 10,
-    verbose: bool = False,
-):
-
-    train_data = TokenizedBookCorpusOpenSplit(train_data_path)
-    validation_data = TokenizedBookCorpusOpenSplit(validation_data_path)
-
-    train_loader = torch.utils.data.DataLoader(
-        train_data, batch_size=batch_size, shuffle=True, collate_fn=TokenizedBookCorpusOpenSplit.collate_batch,
-    )
-    validation_loader = torch.utils.data.DataLoader(
-        validation_data, batch_size=batch_size, shuffle=False, collate_fn=TokenizedBookCorpusOpenSplit.collate_batch,
-    )
-
-    tokenizer = OpenAIGPTTokenizerFast.from_pretrained(tokenizer_path)
-    model = GPT1Pretrain(
-        n_vocab=tokenizer.vocab_size,
-        n_tokens=sequence_length,
-        dim_embed=dim_embed,
-        n_decoder_layers=n_decoder_layers,
-        n_heads=n_heads,
-        dropout=dropout,
-        id="GPT1Pretrain",
-    )
-    model.set_io_verbosity(verbose)
-    model.to(0)
-    loss_fn = torch.nn.CrossEntropyLoss().to(0)
-    optimizer = torch.optim.AdamW(
-        model.parameters(), lr=0.00025, 
-    )
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(
-        optimizer=optimizer,
-        max_lr=0.00025,
-        total_steps=250000,
-        anneal_strategy='cos',
-        pct_start=0.10
-    )
-    timestamp = dt.now().strftime("%Y%m%d_%H%M%S")
+    model: GPT1Pretrain
+    optimizer: torch.optim.Optimizer
+    scheduler: Optional[torch.optim.lr_scheduler.LRScheduler] = None
     summary_writer = torch.utils.tensorboard.writer.SummaryWriter(
-        "runs/gpt1_pretrain_{}".format(timestamp)
+        "runs/gpt1_pretrain_{}".format(dt.now().strftime("%Y%m%d_%H%M%S"))
     )
+    
+    def __init__(self, config: Dict):
+        self.config = config
+    
+    def tb_log(self,*args,**kwargs):
+        self.summary_writer.add_scalar(*args,**kwargs)
 
-    for epoch in range(n_epochs):
-        print(f"EPOCH {epoch + 1}:")
+    def log_step(self, epoch_index: int, batch_index: int, batch_size: int, n_batches: int, step_loss: float):
+        """Displays step level progress on console and logs to tensorboard.
 
-        # run training and retrieve loss averaged over last 1000 batches of
-        # training split
-        train_loss, validation_loss = train_epoch(
-            epoch,
-            train_loader,
-            validation_loader,
-            model,
-            loss_fn,
-            optimizer,
-            scheduler,
-            summary_writer,
-            display_step,
+        Args:
+            epoch_index (int): The index of the current epoch
+            batch_index (int): The index of the current batch
+            batch_size (int): The batch size used during training
+            n_batches (int): The number of batches in one epoch
+            step_loss (float): The loss averaged over the last `step_size`
+                batches.
+        """
+        n_records = n_batches * batch_size
+        record_index_start = batch_index * batch_size
+        record_index_end = record_index_start + batch_size
+        
+        print(
+            f"{now()} | Batch {batch_index+1}/{n_batches}"
+            f" (observations {record_index_start}-{record_index_end}/"
+            f"{n_records}) loss: {step_loss}"
         )
 
+        global_batch_index = (
+            epoch_index * n_batches + batch_index + 1
+        )
+        self.tb_log(
+            "Batch train loss", step_loss, global_batch_index
+        )
+        gpu = GPUtil.getGPUs()[0]
+        self.tb_log(
+            "Batch GPU utilization",
+            {"Load": gpu.load, "Memory": gpu.memoryUtil},
+            global_batch_index,
+        )
+
+    def log_epoch(self, epoch_index: int, train_loss: float, validation_loss: float):
+        """Displays epoch level progress on console and logs to tensorboard.
+
+        Args:
+            epoch_index (int): The index of the current epoch
+            train_loss (float): The training loss for the current epoch
+            validation_loss (float): The validation loss for the current epoch
+        """
+        
         # display and log evaluation data
         print(
             f"{now()} | Train loss {train_loss} for last step of epoch"
-            f" {epoch+1}"
+            f" {epoch_index+1}"
         )
         print(
-            f"{now()} | Validation loss {validation_loss} for epoch {epoch+1}"
+            f"{now()} | Validation loss {validation_loss} for epoch {epoch_index+1}"
         )
 
-        summary_writer.add_scalars(
+        self.tb_log(
             "Epoch train vs. validation loss",
             {"Training": train_loss, "Validation": validation_loss},
-            epoch + 1,
+            epoch_index + 1,
         )
-        summary_writer.flush()
+        self.summary_writer.flush()
+
+    def run_epoch(
+        self,
+        data_loader: torch.utils.data.DataLoader,
+        train: bool = True,
+        epoch_index: int = 0,
+    ) -> float:
+
+        assert self.model is not None
+
+        if train:
+            assert self.optimizer is not None
+            assert self.scheduler is not None
+            self.model.train()
+            self.model.init_weights()
+        else:
+            self.model.eval()
+
+        n_batches = len(data_loader)
+        batch_size = data_loader.batch_size
+        display_step = self.config['training']["display_step"]
+
+        total_loss = 0.0
+        step_loss = 0.0
+        running_loss = 0.0
+
+        # Here, we use enumerate(training_loader) instead of
+        # iter(training_loader) so that we can track the batch
+        # index and do some intra-epoch reporting
+        for batch_index, (inputs, mask, target) in enumerate(data_loader):
+
+            # Every data instance is an input + label pair
+            inputs = inputs.to(0)
+            mask = mask.to(0)
+            target = target.to(0)
+
+            # Zero your gradients for every batch!
+            self.optimizer.zero_grad()
+
+            # Make predictions for this batch
+            logits, loss = self.model(inputs, mask, target)
+
+            if train:
+                # Compute the loss and its gradients
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(),max_norm=1)
+
+                # update model weights and learning rate
+                self.optimizer.step()
+                self.scheduler.step()
+
+            # Gather data and report
+            running_loss += loss.item()
+
+            if batch_index % display_step == (display_step - 1):
+
+                # average loss over last display_step batches
+                step_loss = running_loss / display_step
+                total_loss += step_loss
+
+                self.log_step(epoch_index,batch_index,batch_size,n_batches,step_loss)
+
+                running_loss = 0.0
+
+        return total_loss
+
+    def train(
+            self,
+            model: GPT1Pretrain,
+            train_loader: torch.utils.data.DataLoader,
+            validation_loader: Optional[torch.utils.data.DataLoader] = None
+        ):
+        """Main training loop.
+
+        Args:
+            model (GPT1Pretrain): _description_
+            train_loader (torch.utils.data.DataLoader): _description_
+            validation_loader (torch.utils.data.DataLoader): _description_
+        """
+
+        # set attributes needed for training
+        self.model = model
+        self.optimizer = torch.optim.AdamW(
+            model.parameters(), 
+            **self.config['optimizer']
+        )
+        self.scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            optimizer=self.optimizer,
+            **self.config['scheduler']
+        )
+
+        for epoch_index in range(self.config['training']["n_epochs"]):
+            print(f"EPOCH {epoch_index + 1}:")
+
+            # run training and retrieve loss averaged over last 1000 batches of
+            # training split
+            train_loss = self.run_epoch(
+                data_loader=train_loader,
+                train=True,
+                epoch_index=epoch_index,
+            )
+
+            if validation_loader is not None:
+                validation_loss = self.run_epoch(
+                    data_loader=validation_loader,
+                    train=False,
+                    epoch_index=epoch_index
+                )
+            else:
+                validation_loss = -1
+
+            self.log_epoch(epoch_index, train_loss, validation_loss)
+
+def pretrain(
+    pretrain_config: GPT1PretrainConfig = GPT1PretrainConfig.from_file(),
+    # # dataset
+    # train_data_path: str,
+    # validation_data_path: str,
+    # # dataloader
+    # batch_size: int,
+    # # tokenizer
+    # tokenizer_path: str,
+    # # model
+    # sequence_length: int = 512,
+    # dim_embed: int = 768,
+    # n_decoder_layers: int = 12,
+    # n_heads: int = 12,
+    # dropout: float = 0.1,
+    # # optimizer
+    # learning_rate: float = 0.00025,
+    # weight_decay: float = 0.01,
+    # # training
+    # n_epochs: int = 10,
+    # display_step: int = 10,
+    # verbose: bool = False,
+):
+
+    train_data = TokenizedBookCorpusOpenSplit(pretrain_config['data']['train']['path'])
+    validation_data = TokenizedBookCorpusOpenSplit(pretrain_config['data']['validation']['path'])
+
+    train_loader = torch.utils.data.DataLoader(
+        train_data, collate_fn=TokenizedBookCorpusOpenSplit.collate_batch, **pretrain_config['data']['train']['dataloader'],
+    )
+    validation_loader = torch.utils.data.DataLoader(
+        validation_data, collate_fn=TokenizedBookCorpusOpenSplit.collate_batch, **pretrain_config['data']['validation']['dataloader'],
+    )
+
+    tokenizer = OpenAIGPTTokenizerFast.from_pretrained(pretrain_config['tokenizer']['path'])
+    gpt1 = GPT1Pretrain(
+        n_vocab=tokenizer.vocab_size,
+        **pretrain_config['model']['architecture'],
+        id="GPT1Pretrain",
+    )
+    gpt1.set_io_verbosity(pretrain_config['model']['misc']['verbose'])    
+
+    trainer = GPT1Trainer(pretrain_config['trainer'])
+
+    trainer.train(
+        model=gpt1,
+        train_loader=train_loader,
+        validation_loader=validation_loader,
+    )
